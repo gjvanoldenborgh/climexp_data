d02c9f0a18ecd8faca4e86610c5b930c130320f1
Geert Jan van Oldenborgh <oldenborgh@knmi.nl>
2014-05-05 11:01:17 +0200

    Avoid huge log files :-(

---
+++ GRACEData/update.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+# See mail form Bert Wouters
+#   land
+base=ftp://podaac.jpl.nasa.gov/allData/tellus/L3/land_mass/RL05/netcdf/
+file=\*CSR\*.nc
+wget -q -N "$base/$file"
+thisfile=`ls -t $file | head -1`
+if [ -s "$thisfile" ]; then
+	landfile=$thisfile
+fi
+
+#   ocean
+base=ftp://podaac.jpl.nasa.gov/allData/tellus/L3/ocean_mass/RL05/netcdf/
+file=\*CSR\*.nc
+wget -q -N "$base/$file"
+thisfile=`ls -t $file | head -1`
+if [ -s "$thisfile" ]; then
+    oceanfile=$thisfile
+fi
+
+# this gets rid of the irregular time axis - missing months are set to undefined 
+# and non-centered months are assumed centered.
+cdo inttime,2003-01-17,0:00,1day $landfile grace_land_daily.nc
+daily2longerfield grace_land_daily.nc 12 mean minfac 0.25 grace_land.nc
+$HOME/NINO/copyfiles.sh grace_land.nc
+rm grace_land_daily.nc
+
+cdo inttime,2003-01-17,0:00,1day $oceanfile grace_ocean_daily.nc
+daily2longerfield grace_ocean_daily.nc 12 mean minfac 0.25 grace_ocean.nc
+$HOME/NINO/copyfiles.sh grace_ocean.nc
+rm grace_ocean_daily.nc
+
+$HOME/copyfiles.sh grace_land.nc grace_ocean.nc

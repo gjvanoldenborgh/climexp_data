821696901248c0483be3490afd8a2f7b2efaea04
Geert Jan van Oldenborgh <oldenborgh@knmi.nl>
2014-06-13 14:43:55 +0200

    It seems I had forgotten to add these...

---
+++ UKMOData/update_amo.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+# 1. Definition of van Oldenborgh et al, OS, 2009
+
+version=3.1.0.0
+get_index HadSST.${version}.median.nc -75 -7 25 60 > hadsst_natl.dat
+correlate hadsst_natl.dat file hadcrut3_gl.dat mon 1:12 plot aap.dat
+a=`awk '{print -$10}' aap.dat | tr '\n' ':'`
+export FORM_a1=1
+export FORM_a2=$a
+gen_time 1700 2200 12 > dummy.12.dat
+addseries dummy.12.dat file hadsst_natl.dat file hadcrut4110_ns_avg.dat mon 1:12 plot aap.dat > /tmp/addseries.log
+file=`tail -1 /tmp/addseries.log`
+normdiff $file null none none > aap.dat
+cat > amo_hadsst.dat <<EOF
+# AMO index SST 25-60N, 7-75W minus regression on global mean temperature
+# as in <a haref="http://www.ocean-sci.net/5/293/2009/os-5-293-2009.html">van Oldenborgh et al 2009</a>
+# based on HadSST $version from UKMO
+# AMO [K] SST anomalies
+EOF
+fgrep -v '#' aap.dat >> amo_hadsst.dat
+rm $file aap.dat
+$HOME/NINO/copyfiles.sh amo_hadsst.dat
+
+# 2. Defnbition of Trenberth & Shea 2006
+
+get_index HadSST.${version}.median.nc -80 0 0 60 > hadsst_0-60N_0-80W.dat
+get_index HadSST.${version}.median.nc 0 360 -60 60 > hadsst_60S-60N.dat
+cat > amo_hadsst_ts.dat <<EOF
+# AMO index SST EQ-60N, 0-80W minus SST 60S-60N
+# as in <a href="http://www.agu.org/pubs/crossref/2006/2006GL026894.shtml">Trenberth and Shea 2006</a>
+# based on HadSST ${version} from UKMO
+# AMO [C] SST
+EOF
+normdiff hadsst_0-60N_0-80W.dat hadsst_60S-60N.dat none none | egrep -v '^#' >> amo_hadsst_ts.dat
+$HOME/NINO/copyfiles.sh amo_hadsst_ts.dat

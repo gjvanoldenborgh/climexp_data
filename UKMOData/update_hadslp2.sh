bb5a0dd9bdf0dfc31ea6c9749de05eef83337868
Geert Jan van Oldenborgh <oldenborgh@knmi.nl>
2014-06-23 13:59:49 +0200

    Updated URL

--- UKMOData/update_hadslp2.sh
+++ UKMOData/update_hadslp2.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-wget -N --header="accept-encoding: gzip" http://www.metoffice.gov.uk/hadobs/gmslp/hadslp2/data/hadslp2r.asc.gz
+wget -N --header="accept-encoding: gzip" http://www.metoffice.gov.uk/hadobs/hadslp2/data/hadslp2r.asc.gz
 gunzip -c hadslp2r.asc.gz > hadslp2r.asc
 wget -N --header="accept-encoding: gzip" http://www.metoffice.gov.uk/hadobs/hadslp2/data/hadslp2.0_acts.asc.gz
 gunzip -c hadslp2.0_acts.asc.gz > hadslp2.0_acts.asc

beaa981df359525781d73732b7c71d64e024dfa3
Geert Jan van Oldenborgh <oldenborgh@knmi.nl>
2014-11-17 21:34:12 +0100

    Extend monthly 0.5º E-OBS backwards with the CRU TS data

---
+++ ENSEMBLES/merge_with_cru.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+version=v10.0
+cversion=3.22
+for var in tg
+do
+    file=${var}_0.50deg_reg_${version}u.nc
+    monfile=${var}_0.50deg_reg_${version}u_mon.nc
+    outfile=${var}_0.50deg_reg_${version}u_extended.nc
+    case $var in
+        tg) ccfile=`ls -t ../CRUData/cru_ts$cversion.1901.2???.tmp.dat.nc | head -1`;;
+        *) echo "cannot handle $var yet"; exit -1;;
+    esac
+    if [ -z "$ccfile" -o ! -s "$ccfile" ]; then
+        echo "cannot find CRU file $ccfile"
+        exit -1
+    fi
+    cfile=`basename $ccfile`
+    if [ ! -s $cfile -o $cfile -ot ../CRUData/$cfile ]; then
+        cdo sellonlatbox,-40.5,75.5,25.,75.5 ../CRUData/$cfile ./$cfile
+    fi
+    if [ ! -s $monfile -o $monfile -ot $file ]; then
+        daily2longerfield $file 12 mean $monfile
+    fi
+    if [ ! -s $outfile -o $outfile -ot $monfile -o $outfile -ot $cfile ]; then
+        patchfield $monfile $cfile $outfile
+    fi
+done

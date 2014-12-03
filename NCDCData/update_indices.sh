db7f49f7859754dc7c9a867df8fe3200f60335bf
Geert Jan van Oldenborgh <oldenborgh@knmi.nl>
2014-12-03 14:33:30 +0100

    NCDC has changed everything :-(

--- NCDCData/update_indices.sh
+++ NCDCData/update_indices.sh
@@ -1,42 +1,39 @@
 #!/bin/sh
-
-base=ftp://ftp.ncdc.noaa.gov/pub/data/anomalies/
-
-# to speed up the script, NCDC delays every login
-wget -q -N $base/monthly\*dat
+# http://www.ncdc.noaa.gov/cag/time-series/global/globe/land_ocean/p12/12/1880-2014.csv
+# http://www.ncdc.noaa.gov/cag/time-series/global/globe/land/p12/12/1880-2014.csv
+# http://www.ncdc.noaa.gov/cag/time-series/global/globe/ocean/p12/12/1880-2014.csv
+base=http://www.ncdc.noaa.gov/cag/time-series/global
+yrnow=`date -d "1 month ago" "+%Y"`
 
 files=""
-for region in gl nh sh
+for region in gl # nh sh are not available any more
 do
-  case $region in
-  gl) regionname=global;;
-  nh) regionname="Northern Hemisphere";;
-  sh) regionname="Southern Hemisphere";;
-  esac
-  for area in land ocean land_ocean
-  do
     case $region in
-    gl) file=monthly.${area}.90S.90N.df_1901-2000mean.dat;;
-    nh) file=monthly.${area}.00N.90N.df_1901-2000mean.dat;;
-    sh) file=monthly.${area}.90S.00N.df_1901-2000mean.dat;;
+        gl) dir=globe;regionname=global;;
+        nh) regionname="Northern Hemisphere";;
+        sh) regionname="Southern Hemisphere";;
     esac
-###    cp $file $file.old
-###    wget -N $base$file
-    if [ $area = land_ocean ]; then
-      myfile=ncdc_${region}.dat
-    else
-      myfile=ncdc_${region}_${area}.dat
-    fi
-    cat > $myfile <<EOF
-# $regionname $area mean temperature anomalies from <a href="http://lwf.ncdc.noaa.gov/oa/climate/research/anomalies/anomalies.html">NCDC</a>
-# Ta [K] surface temperature
+    for area in land ocean land_ocean
+    do
+        file=1880-$yrnow.csv
+        [ -f $file ] && rm $file # they all have the same name...
+        echo wget $base/$dir/$area/p12/12/$file
+        wget $base/$dir/$area/p12/12/$file
+        if [ $area = land_ocean ]; then
+            myfile=ncdc_${region}.dat
+        else
+            myfile=ncdc_${region}_${area}.dat
+        fi
+        cat > $myfile <<EOF
+# $regionname $area mean temperature anomalies from <a href="http://www.ncdc.noaa.gov/monitoring-references/faq/anomalies.php">NCDC</a>
+# Ta [K] surface temperature anomaly
 EOF
-    cat $file >> $myfile
-    yrfile=`basename $myfile .dat`_yr.dat
-    daily2longer $myfile 1 mean > $yrfile
-    files="$files $myfile $yrfile"
-    plotdat $yrfile > `basename $yrfile .dat`.txt
-  done
+        egrep '^[12]' $file | tr ',' ' ' >> $myfile
+        yrfile=`basename $myfile .dat`_yr.dat
+        daily2longer $myfile 1 mean > $yrfile
+        files="$files $myfile $yrfile"
+        plotdat $yrfile > `basename $yrfile .dat`.txt
+    done
 done
 $HOME/NINO/copyfilesall.sh $files
 

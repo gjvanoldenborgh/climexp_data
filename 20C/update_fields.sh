5ed69af53985abf794d1a61814fb1f3f032b2532
Geert Jan van Oldenborgh <oldenborgh@knmi.nl>
2015-02-18 10:15:02 +0100

    Extend 20C SLP with NCEP/NCAR R1

---
+++ 20C/update_fields.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+for var in prmsl
+do
+    case $var in
+        prmsl) nvar=slp;;
+        *) echo "$0: error: unknown var $var"; exit -1;;
+    esac
+    cfile=$var.mon.mean.nc
+    nnfile=$nvar.mon.mean.nc
+    nfile=$HOME/NINO/NCEPNCAR40/$nnfile
+    efile=$var.mon.mean_extended.nc
+
+    cdo remapbil,$cfile $nfile $nnfile
+    patchfield $cfile $nnfile $efile
+    $HOME/NINO/copyfiles.sh $efile
+done

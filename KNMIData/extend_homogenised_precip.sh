e157a90f686887821a4cecf1af8a52ddea191401
Geert Jan van Oldenborgh <oldenborgh@knmi.nl>
2015-08-28 17:19:57 +0200

    Extend homogenised series to now (request by Hans Visser)

---
+++ KNMIData/extend_homogenised_precip.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#
+# attach real-time data from the 8-8 network to Adri's homogenised precipitation dataset 
+# that only runs up to 2009.
+#
+for file in precip???_hom_1910-2009.dat.gz precip???_hom_1951-2009.dat.gz
+do
+    id=${file#precip}
+    id=${id%%_*}
+    oper=rr$id.gz
+    if [ -s $oper ]; then
+        newfile=${file%2009.dat.gz}now.dat
+        echo "# extended with operational data from KNMI" > $newfile
+        zcat $file >> $newfile
+        zcat $oper | egrep '^20[1234]' >> $newfile
+        gzip -f $newfile
+    fi
+done

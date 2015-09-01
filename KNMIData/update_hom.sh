96f86a2166708294af1d784e9de73dca9c3c05ae
Geert Jan van Oldenborgh <oldenborgh@knmi.nl>
2015-09-01 14:09:50 +0200

    New web server...

--- KNMIData/update_hom.sh
+++ KNMIData/update_hom.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 mv tg_hom_mnd260.txt tg_hom_mnd260.txt.old
-wget http://www.knmi.nl/klimatologie/onderzoeksgegevens/homogeen_260/tg_hom_mnd260.txt
+wget http://projects.knmi.nl/klimatologie/onderzoeksgegevens/homogeen_260/tg_hom_mnd260.txt
 diff tg_hom_mnd260.txt.old tg_hom_mnd260.txt
 if [ $? = 0 ]; then
   echo no changes
   mv tg_hom_mnd260.txt.old tg_hom_mnd260.txt
   exit
 fi
-./homtxt2dat < tg_hom_mnd260.txt  tr -s '' > Tdebilt_hom.dat
+./homtxt2dat < tg_hom_mnd260.txt  tr -s '
+' > Tdebilt_hom.dat
 $HOME/NINO/copyfiles.sh Tdebilt_hom.dat

34d623f62936fa491ceb3b40e8aa9d3d652c9f37
Geert Jan van Oldenborgh <oldenborgh@knmi.nl>
2015-12-14 20:59:17 +0100

    Better way to handle this.

--- RutgersData/update_fields.sh
+++ RutgersData/update_fields.sh
@@ -3,7 +3,7 @@ force=""
 [ -n "$1" ] && force=$1
 file=rutgers-monthly-snow.mtx
 cp $file.gz $file.gz.old
-wget -N --user=knmi --password=bIr19pFS http://climate.rutgers.edu/snowcover/files/$file.gz
+wget -N --user=knmi http://climate.rutgers.edu/snowcover/files/$file.gz
 cmp $file.gz $file.gz.old
 if [ $? != 0 -o "$force" = force ]; then
     gunzip -c $file.gz > $file

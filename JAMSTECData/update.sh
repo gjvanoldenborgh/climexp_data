613532f2c75e7f3d13736d1c3fe893451130e5c2
Geert Jan van Oldenborgh <oldenborgh@knmi.nl>
2016-06-26 15:21:38 +0200

    Updated URL Switched East and West to what they should be.

--- JAMSTECData/update.sh
+++ JAMSTECData/update.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 wget -N http://www.jamstec.go.jp/frcgc/research/d1/iod/DATA/dmi.monthly.txt
-for region in seio wio siod
+for region in seio wio dmi
 do
     case $region in
-        seio) col=5;name="East Indian Ocean";;
-        wio) col=6;name="West Indian Ocean";;
-        siod) col=7;name="DMI (WIO-SEIO)";;
+        seio) col=6;name="East Indian Ocean";;
+        wio) col=5;name="West Indian Ocean";;
+        dmi) col=7;name="DMI (WIO-SEIO)";;
         *) echo "$0: error bcguwyoageylucw"; exit -1;;
     esac
     echo "# $region [Celsius] $name SST anomalies" > $region.dat
-    echo "# from <a href=http://www.jamstec.go.jp/frcgc/research/d1/iod/e/iod/about_iod.html>JAMSTEC</a>" >> $region.dat
+    echo "# from <a href=http://www.jamstec.go.jp/frcgc/research/d1/iod/e/index.html>JAMSTEC</a>" >> $region.dat
     tail -n +2 dmi.monthly.txt | tr ":" " " | cut -d " " -f 1,2,$col >> $region.dat
 done
 $HOME/NINO/copyfiles.sh *.dat

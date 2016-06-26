e12f66c852bad3a3304ff5dcc411c9ec0158e387
Geert Jan van Oldenborgh <oldenborgh@knmi.nl>
2016-06-26 15:22:18 +0200

    Updated the areas to the offical definitions Made anomalies

--- NCDCData/makeiozm.sh
+++ NCDCData/makeiozm.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
-get_index ersstv4.nc 90 100 -10 0 > seio_ersst.dat
-get_index ersstv4.nc 40 80 -15 15 > wio_ersst.dat
-normdiff wio_ersst.dat seio_ersst.dat none none > dmi_ersst.dat
+get_index ersstv4.nc 90 110 -10 0 > aap.dat
+plotdat anom 1982 2005 aap.dat | fgrep -v repeat > seio_ersst.dat
+get_index ersstv4.nc 50 70 -10 10 > aap.dat
+plotdat anom 1982 2005 aap.dat | fgrep -v repeat > wtio_ersst.dat
+normdiff wtio_ersst.dat seio_ersst.dat none none > dmi_ersst.dat

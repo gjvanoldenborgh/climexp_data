9c3d9068fb87f89fb7197e58d67765abd86f9b15
Geert Jan van Oldenborgh <oldenborgh@knmi.nl>
2016-09-07 11:21:58 +0200

    Oops.

--- UCSBData/update.sh
+++ UCSBData/update.sh
@@ -8,7 +8,6 @@ base=ftp://ftp.chg.ucsb.edu/pub/org/chg/products/CentennialTrends
 file=CenTrends_v1_monthly.nc
 cp $file $file.old
 wget -q -N $base/$file
-exit
 cmp $file $file.old
 if [ $? != 0 -o $force = true ]; then
     cdo -r -f nc4 -z zip -selvar,precip -settaxis,1900-01-15,0:00,1month $file CenTrends_v1_monthly_ce.nc

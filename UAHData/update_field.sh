e1ce86d789e740ff6e8340d890cc18fa652e417f
Geert Jan van Oldenborgh <oldenborgh@knmi.nl>
2017-01-11 17:14:21 +0100

    Completely update to 6.0

--- UAHData/update_field.sh
+++ UAHData/update_field.sh
@@ -1,5 +1,43 @@
 #!/bin/sh
 yr=1979
+version="6.0"
+myversion="60"
+base=http://www.atmos.uah.edu/public/msu/v6.0/tlt/
+yrnow=`date -d "last month" +"%Y"`
+while [ $yr -le $yrnow ]
+do
+  file=tltmonamg.${yr}_$version
+  wget -q -N $base$file
+  size=`wc -c $file | awk '{print $1}'`
+  if [ -n "$size" -a ${size:-0} -lt 500 ]
+  then
+    echo "$0: error retrieving $base$file"
+    rm $file
+    yr=3000
+  fi
+  yr=$(($yr + 1))
+done
+
+make msu2grads
+./msu2grads $version
+grads2nc tlt.ctl tlt.nc
+mv tlt.nc tlt_$myversion.nc
+
+cp tlt_$myversion.nc tlt.nc
+
+get_index tlt.nc 0 360 -90 90 > tlt_gl.dat
+get_index tlt.nc 0 360 -90 0 > tlt_sh.dat
+get_index tlt.nc 0 360 0 90 > tlt_nh.dat
+get_index tlt.nc 0 360 -90 90 lsmask lsmask_25_180.nc 5lan > tlt_land.dat
+get_index tlt.nc 0 360 -90 90 lsmask lsmask_25_180.nc 5sea > tlt_sea.dat
+###. ./update_indices.sh
+
+$HOME/NINO/copyfiles.sh tlt*.nc tlt_??.dat tlt_land.dat tlt_sea.dat
+
+exit 
+
+# old versions
+yr=1979
 for version in "5.6" "5.5"
 do
 base=http://www.atmos.uah.edu/public/msu/t2lt/
@@ -34,41 +72,3 @@ case $version in
     5.6) mv tlt.nc tlt_56.nc;;
 esac
 done # version
-
-yr=1979
-for beta in 5
-do
-version="6.0beta$beta"
-myversion="60b$beta"
-base=http://www.atmos.uah.edu/public/msu/v6.0beta/tlt/
-yrnow=`date -d "last month" +"%Y"`
-while [ $yr -le $yrnow ]
-do
-  file=tltmonamg.${yr}_$version
-  wget -q -N $base$file
-  size=`wc -c $file | awk '{print $1}'`
-  if [ -n "$size" -a ${size:-0} -lt 500 ]
-  then
-    echo "$0: error retrieving $base$file"
-    rm $file
-    yr=3000
-  fi
-  yr=$(($yr + 1))
-done
-
-make msu2grads
-./msu2grads $version
-grads2nc tlt.ctl tlt.nc
-mv tlt.nc tlt_$myversion.nc
-done # version
-
-cp tlt_$myversion.nc tlt.nc
-
-get_index tlt.nc 0 360 -90 90 > tlt_gl.dat
-get_index tlt.nc 0 360 -90 0 > tlt_sh.dat
-get_index tlt.nc 0 360 0 90 > tlt_nh.dat
-get_index tlt.nc 0 360 -90 90 lsmask lsmask_25_180.nc 5lan > tlt_land.dat
-get_index tlt.nc 0 360 -90 90 lsmask lsmask_25_180.nc 5sea > tlt_sea.dat
-###. ./update_indices.sh
-
-$HOME/NINO/copyfiles.sh tlt*.nc tlt_??.dat tlt_land.dat tlt_sea.dat

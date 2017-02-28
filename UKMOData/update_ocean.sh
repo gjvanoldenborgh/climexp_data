0256a215505ea1f82f0268b853dff43de8952343
Geert Jan van Oldenborgh <oldenborgh@knmi.nl>
2017-02-28 15:57:59 +0100

    Update EN to 4.2.0

--- UKMOData/update_ocean.sh
+++ UKMOData/update_ocean.sh
@@ -2,8 +2,8 @@
 set -x
 # download the EN4 objective ocean analysis
 
-base=http://www.metoffice.gov.uk/hadobs/en4/data/en4-1-1/
-version=EN.4.1.1
+base=http://www.metoffice.gov.uk/hadobs/en4/data/en4-2-0/
+version=EN.4.2.0
 mem=500
 
 yr=1899

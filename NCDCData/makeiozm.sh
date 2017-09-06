ad547bcc95a1c02839243b4355a1e037f6edb2e3
Geert Jan van Oldenborgh <oldenborgh@knmi.nl>
2017-09-06 11:54:47 +0200

    Added ERSST v5. Prepared for v6.

--- NCDCData/makeiozm.sh
+++ NCDCData/makeiozm.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
-get_index ersstv4.nc 90 110 -10 0 > aap.dat
+if [ -z "$version" ]; then
+    echo "$0: error: version unset"
+    exit
+fi
+
+get_index ersst${version}.nc 90 110 -10 0 > aap.dat
 plotdat anom 1982 2005 aap.dat | fgrep -v repeat > seio_ersst.dat
-get_index ersstv4.nc 50 70 -10 10 > aap.dat
+get_index ersst${version}.nc 50 70 -10 10 > aap.dat
 plotdat anom 1982 2005 aap.dat | fgrep -v repeat > wtio_ersst.dat
 normdiff wtio_ersst.dat seio_ersst.dat none none > dmi_ersst.dat
--- NCDCData/makesiod.sh
+++ NCDCData/makesiod.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
-get_index ersstv4.nc 90 100 -28 -18 > esiod_ersst.dat
-get_index ersstv4.nc 55 65 -37 -27 > wsiod_ersst.dat
+if [ -z "$version" ]; then
+    echo "$0: error: version unset"
+    exit
+fi
+
+get_index ersst${version}.nc 90 100 -28 -18 > esiod_ersst.dat
+get_index ersst${version}.nc 55 65 -37 -27 > wsiod_ersst.dat
 normdiff wsiod_ersst.dat esiod_ersst.dat none monthly > siod_ersst.dat

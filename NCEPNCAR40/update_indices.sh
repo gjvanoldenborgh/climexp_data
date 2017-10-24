1ae69222c32581565cfbf511230b8faec2d7fd64
Geert Jan van Oldenborgh <oldenborgh@knmi.nl>
2017-10-24 09:21:18 +0200

    QBO index had moved more than a year ago...

--- NCEPNCAR40/update_indices.sh
+++ NCEPNCAR40/update_indices.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 cp qbo.data qbo.data.old
-wget -N http://www.cdc.noaa.gov/Correlation/qbo.data
+wget -N --no-check-certificate https://www.esrl.noaa.gov/psd/data/correlation/qbo.data
 mv nqbo.dat nqbo.dat.old
 cat << EOF > nqbo.dat
-# QBO index by Cathy Smith from the NCEP/NCAR reanalysis
-# available from <a href="http://www.cdc.noaa.gov/ClimateIndices/List/#QBO">CDC</a>
-# last updated: `date +"%Y-%m-%d"`
+# QBO index by PSD from  the zonal average of the 30mb zonal wind at the equator in the NCEP/NCAR reanalysis
+# available from <a href="https://www.esrl.noaa.gov/psd/data/climateindices/list/#QBO">ESRL</a>
+# QBO [1] QBO index
 EOF
 egrep -v '^ ' qbo.data | sed -e 's/999.0/999.9/g' >> nqbo.dat
 $HOME/NINO/copyfiles.sh nqbo.dat

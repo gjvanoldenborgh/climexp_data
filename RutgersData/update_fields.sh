cc60275f77df46ebbde8e5d0d45a52a1105e5b36
Geert Jan van Oldenborgh <oldenborgh@knmi.nl>
2017-10-24 09:22:07 +0200

    Changed to https, and KNMI does nt like that.

--- RutgersData/update_fields.sh
+++ RutgersData/update_fields.sh
@@ -3,7 +3,7 @@ force=""
 [ -n "$1" ] && force=$1
 file=rutgers-monthly-snow.mtx
 cp $file.gz $file.gz.old
-wget -N --user=knmi http://climate.rutgers.edu/snowcover/files/$file.gz
+wget -N --no-check-certificate --user=knmi https://climate.rutgers.edu/snowcover/files/$file.gz
 cmp $file.gz $file.gz.old
 if [ $? != 0 -o "$force" = force ]; then
     gunzip -c $file.gz > $file

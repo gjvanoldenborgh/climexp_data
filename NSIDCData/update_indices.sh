f2b5d4374da29a21b38ef590d513ced2349232e2
Geert Jan van Oldenborgh <oldenborgh@knmi.nl>
2017-11-19 20:30:39 +0100

    NMoved from v2.1 to 33.0, made future updates easier.

--- NSIDCData/update_indices.sh
+++ NSIDCData/update_indices.sh
@@ -2,7 +2,7 @@
 # get sea ice indices from NSIDC and convert to my data type
 
 base=ftp://sidads.colorado.edu/DATASETS/NOAA/G02135/
-
+version=v3.0
 m=0
 while [ $m -lt 12 ]
 do
@@ -13,11 +13,11 @@ do
 	    mo=$m
     fi
 
-    wget -q -N $base/north/monthly/data/N_${mo}_extent_v2.1.csv
-    wget -q -N $base/south/monthly/data/S_${mo}_extent_v2.1.csv
+    wget -q -N $base/north/monthly/data/N_${mo}_extent_$version.csv
+    wget -q -N $base/south/monthly/data/S_${mo}_extent_$version.csv
 done
 
 make txt2dat
-./txt2dat
+./txt2dat $version
 
 $HOME/NINO/copyfilesall.sh ?_ice_extent.dat ?_ice_area.dat

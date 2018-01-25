10252b9e06ccd70363e32c218be2a8d7d7b3e48b
Geert Jan van Oldenborgh <oldenborgh@knmi.nl>
2018-01-25 13:39:08 +0100

    Added daily windspeewd and max windspeed for wind mills and storms Added downword solar radiation for solar panels Added net surface radiation for Penman potential evaporation (to come)

--- ERA-interim/make_wspd.sh
+++ ERA-interim/make_wspd.sh
@@ -4,6 +4,7 @@ yr=1979
 mo=1
 mm=`printf %02i $mo`
 files=""
+maxfiles=""
 while [ -s u10$yr.grib -o -s u10$yr$mm.grib ]; do
     if [ -s u10$yr.grib ]; then
         ufile=u10$yr.grib
@@ -28,16 +29,25 @@ while [ -s u10$yr.grib -o -s u10$yr$mm.grib ]; do
     ext=${ufile#u10}
     ext=${ext%.grib}
     outfile=wspd$ext.nc
-    if [ ! -s $outfile ]; then
+    maxoutfile=maxwspd$ext.nc
+    if [ ! -s $outfile -o ! -s $maxoutfile ]; then
+        echo "making $outfile and $maxoutfile"
         $cdo -shifttime,1hour -sqr $ufile /tmp/utmp$ext.nc
         $cdo -shifttime,1hour -sqr $vfile /tmp/vtmp$ext.nc
         $cdo add /tmp/utmp$ext.nc /tmp/vtmp$ext.nc ./wspd2tmp$ext.nc
         rm /tmp/utmp$ext.nc /tmp/vtmp$ext.nc
         $cdo -sqrt -daymean ./wspd2tmp$ext.nc $outfile
+        $cdo -sqrt -daymax ./wspd2tmp$ext.nc $maxoutfile
         ncrename -v var165,wspd $outfile
-        ncatted -a units,wspd,a,c,"m/s" -a long_name,wspd,a,c,"10m wind speed sqrt(u10^2+v10^2) 3hr" $outfile
+        ncatted -a units,wspd,a,c,"m/s" -a long_name,wspd,a,c,"daily mean of 10m wind speed sqrt(u10^2+v10^2) 3hr" $outfile
+        ncrename -v var165,maxwspd $maxoutfile
+        ncatted -a units,maxwspd,a,c,"m/s" -a long_name,maxwspd,a,c,"daily max of 10m wind speed sqrt(u10^2+v10^2) 3hr" $maxoutfile
         rm ./wspd2tmp$ext.nc
     fi
     files="$files $outfile"
+    maxfiles="$maxfiles $maxoutfile"
 done
+echo "Concatenating into erai_wspd_daily.nc"
 $cdo copy $files erai_wspd_daily.nc
+echo "Concatenating into erai_maxwspd_daily.nc"
+$cdo copy $maxfiles erai_maxwspd_daily.nc

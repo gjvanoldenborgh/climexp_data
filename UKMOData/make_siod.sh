45871f41332caaf73734e840b81f1b9cfa81c88c
Geert Jan van Oldenborgh <oldenborgh@knmi.nl>
2018-01-25 13:33:56 +0100

    Changed HadISST1 file to the netcdf version with more metadata. Adjusted the scripts that use it accordingly.enino.sh

--- UKMOData/make_siod.sh
+++ UKMOData/make_siod.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-get_index hadisst1.ctl 90 100 -28 -18 > hadisst1_esiod.dat
-get_index hadisst1.ctl 55 65 -37 -27 > hadisst1_wsiod.dat
+get_index HadISST_sst.nc 90 100 -28 -18 > hadisst1_esiod.dat
+get_index HadISST_sst.nc 55 65 -37 -27 > hadisst1_wsiod.dat
 normdiff hadisst1_wsiod.dat hadisst1_esiod.dat none none > aap.dat
 scaleseries 1.5 aap.dat > hadisst1_siod.dat
--- UKMOData/update_hadisst1.sh
+++ UKMOData/update_hadisst1.sh
@@ -1,53 +1,20 @@
 #!/bin/sh
-for file in HadISST1_SST_1870-1900.txt HadISST1_SST_1901-1930.txt HadISST1_SST_1931-1960.txt HadISST1_SST_1961-1990.txt HadISST1_SST_1991-2003.txt
-do
-  cp $file.gz $file.gz.old
-  wget -q -N --header="accept-encoding: gzip" http://www.metoffice.gov.uk/hadobs/hadisst/data/$file.gz
-  cmp $file.gz $file.gz.old
-  if [ $? != 0 ]; then
-      c=`file $file.gz | fgrep -c ASCII`
-      if [ $c = 0 ]; then
-	  gunzip -c $file.gz > $file
-      else
-	  cp $file.gz $file
-      fi
-  fi
-  rm $file.gz.old
-done 
+wget="wget --no-check-certificate -q -N"
+cdo="cdo -r -f nc4 -z zip"
+base=http://hadobs.metoffice.gov.uk/hadisst/data/
 
-yr=2004
-now=`date -d "1 month ago" "+%Y"`
-while [ $yr -le $now ]
-do
-  file=HadISST1_SST_$yr.txt
-  cp $file.gz $file.gz.old
-  wget -q -N --header="accept-encoding: gzip" http://www.metoffice.gov.uk/hadobs/hadisst/data/$file.gz
-  cmp $file.gz $file.gz.old
-  if [ $? != 0 ]; then
-      c=`file $file.gz | fgrep -c ASCII`
-      if [ $c = 0 ]; then
-	  gunzip -c $file.gz > $file
-      else
-	  cp $file.gz $file
-      fi
-  fi
-  rm $file.gz.old
-  yr=$((yr + 1))
+for var in sst ice; do
+    case $var in
+        sst) ncvar=sst;;
+        ice) ncvar=sic;;
+    esac
+    echo "wget $base/HadISST_$var.nc.gz"
+    $wget $base/HadISST_$var.nc.gz
+    gunzip -c HadISST_$var.nc.gz > aap.nc
+    $cdo selvar,$ncvar aap.nc HadISST_$var.nc
+    $HOME/NINO/copyfiles.sh HadISST_$var.nc
 done
-
-make hadisst2grads
-./hadisst2grads
-$HOME/NINO/copyfilesall.sh hadisst1.ctl hadisst1.grd
-
-cp HadISST_ice.nc HadISST_ice.nc.old
-wget -N --header="accept-encoding: gzip" http://www.metoffice.gov.uk/hadobs/hadisst/data/HadISST_ice.nc.gz
-c=`file  HadISST_ice.nc.gz | fgrep -c zip`
-if [ $c = 1 ]; then
-    gunzip -c HadISST_ice.nc.gz > HadISST_ice.nc
-else
-    cp HadISST_ice.nc.gz HadISST_ice.nc
-fi
-$HOME/NINO/copyfiles.sh HadISST_ice.nc
+rm aap.nc
 
 ./makenino.sh
 ./make_iod.sh

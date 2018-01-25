7d6aba24ab51e4ec33b4e667da30ada11ca7aabf
Geert Jan van Oldenborgh <oldenborgh@knmi.nl>
2018-01-25 13:36:40 +0100

    Put metadata in my format

--- MeteoSwissData/update.sh
+++ MeteoSwissData/update.sh
@@ -4,7 +4,9 @@ version=10.18751-Climate-Timeseries-CHTM-1.0
 for region in swiss north.low north.high south
 do
     wget -q -N ${base}${version}-$region.txt
-    egrep -v '^[12]' ${version}-$region.txt | fgrep -v 'djf' | tr -d '\r' | egrep -v '^[ \t]*$' | sed -e 's/^/# /' > swiss_$region.dat
+    egrep -v '^[12]' ${version}-$region.txt | fgrep -v 'djf' | tr -d '\r' | egrep -v '^[ \t]*$' \
+        | sed -e 's/^/# /' -e 's/: / :: /' -e 's/DOI: /doi:/' > swiss_$region.dat
+    echo "# T [Celsius] Swiss ${region}-averaged temperature" >> swiss_$region.dat
     egrep '^[12]'  ${version}-$region.txt | cut -f 1-13 -d '	' | sed -e 's/NA/-999.9/g' >> swiss_$region.dat
 done
 $HOME/NINO/copyfiles.sh swiss_*.dat

150e2cb989dad555d900f17a6ac26ce58c948693
Geert Jan van Oldenborgh <oldenborgh@knmi.nl>
2018-02-05 21:00:42 +0100

    Noiw works again

--- GPCCData/merge_telecon.sh
+++ GPCCData/merge_telecon.sh
@@ -19,20 +19,22 @@ fi
 month=0
 files=""
 while [ $month -lt 12 ]; do
-    month=$((month+1))
+    ((month++))
+    echo $month
     outfile=telecon_prcp_$month.dat
     patternfield $file corr_prcp_nino34.nc regr $month > $outfile
     files="$files $outfile"
+    [ $month = 1 ] && firstfile=$outfile
 done
 outfile=telecon_nino34_$dataset.dat
 ./merge_telecon $files > $outfile
-rm $files
 mv $outfile aap.dat
 cat > $outfile <<EOF
 # Nino34 index based on $description projected on ENSO teleconnections
 # using regression patterns
 # nino34_prcp [1] Nino index from land precipitation
 EOF
+head -n 200 $firstfile | fgrep ' :: ' >> $outfile
 normdiff aap.dat null monthly monthly | fgrep -v '#' >> $outfile
 month=0
 files=""
@@ -41,6 +43,7 @@ while [ $month -lt 12 ]; do
     outfile=telecon_corr_prcp_$month.dat
     patternfield $file corr_prcp_nino34.nc corr $month > $outfile
     files="$files $outfile"
+    [ $month = 1 ] && firstfile=$outfile
 done
 outfile=telecon_corr_nino34_$dataset.dat
 ./merge_telecon $files > $outfile
@@ -51,5 +54,6 @@ cat > $outfile <<EOF
 # using correlation patterns
 # nino34_prcp [1] Nino index from land precipitation
 EOF
+head -n 200 $firstfile | fgrep ' :: ' >> $outfile
 normdiff aap.dat null monthly monthly | fgrep -v '#' >> $outfile
 $HOME/NINO/copyfilesall.sh telecon_nino34_$dataset.dat telecon_corr_nino34_$dataset.dat

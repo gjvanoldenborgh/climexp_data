a20f6be11fb13af6a9916b9acaf327fff1b271d8
Geert Jan van Oldenborgh <oldenborgh@knmi.nl>
2018-03-06 20:06:40 +0100

    Converted to f90 Added metadata

---
+++ NASAData/saod2dat.f90
@@ -0,0 +1,54 @@
+program saod2dat
+
+!   convert the GISS stratospheric optical depth file to Climate Explorer conventions
+
+    implicit none
+    integer :: yr,mo,i,j,k,ii(8)
+    real :: fyr,vals(3)
+    character :: reg(3)*2,region(3)*20,history*300
+    data reg /'gl','nh','sh'/
+    data region /'global','northern hemisphere','southern hemisphere'/
+
+    call date_and_time(values=ii)
+    history = '# history :: retrieved and converted'
+    write(history(len_trim(history)+2:),'(i4,a,i2.2,a,i2.2)') ii(1),'-',ii(2),'-',ii(3)
+    write(history(len_trim(history)+2:),'(i2,a,i2.2,a,i2.2)') ii(5),':',ii(6),':',ii(7)
+    do i=1,3
+        open(i,file='saod_'//reg(i)//'.dat')
+        write(i,'(3a)') '# ',trim(region(i)),' Optical Thickness at 550 nm'
+        write(i,'(3a)') '# from <a href="http://data.giss.nasa.gov/' &
+            ,'modelforce/strataer/">NASA/GISS</a>'
+        write(i,'(a)') '# AOD [1] stratospheric aerosol optical depth'
+        write(i,'(a)') '# institution :: NASA/GISS'
+        write(i,'(a)') '# title :: GISS stratospheric aerosol optical depth at 550 nm'
+        write(i,'(a)') '# contact :: https://www.giss.nasa.gov/staff/makiko_sato.html'
+        write(i,'(a)') '# references :: Bourassa, A.E., A. Robock, et al. 2012: '// &
+            'Large volcanic aerosol load in the stratosphere linked to Asian monsoon '// &
+            'transport. Science 337, 78-81, doi:10.1126/science.1219371'
+        write(i,'(a)') '# source_url :: https://data.giss.nasa.gov/modelforce/strataer/'
+        write(i,'(a)') trim(history)
+    end do
+    open(10,file='tau_line.txt',status='old')
+    do i=1,4
+        read(10,'(a)')
+    end do
+    1 do while ( .true. )
+        read(10,*,end=800) fyr,vals
+        yr = int(fyr)
+        mo = 1 + int(12*(fyr-yr))
+        do i=1,3
+            write(i,'(i4,i3,f6.3)') yr,mo,vals(i)
+        end do
+    end do
+800 continue
+!   assume no volcano erupted the last few years...
+    do i=yr,ii(1)
+        do j=1,12
+            if ( i == yr .and. j <= mo ) cycle
+            if ( i == ii(1) .and. j >= ii(2) ) cycle
+            do k=1,3
+                write(k,'(i4,i3,f6.3)') i,j,0.
+            end do
+        end do
+    end do
+end program saod2dat
---
+++ NASAData/txt2dat.f90
@@ -0,0 +1,152 @@
+program txt2dat
+!   convert the GISS txt files to my format dat files
+    implicit none
+    integer,parameter :: yrbeg=1880,yrend=2020
+    integer :: i,j,vals(20),yr,idatum(8)
+    real :: mdata(12,yrbeg:yrend),sdata(4,yrbeg:yrend),adata(yrbeg:yrend)
+    character :: line*300,history*100,reg*3,region*30,type*30,rg*2,tp*2
+    logical :: lwrite
+    integer :: iargc
+
+    lwrite = .false. 
+    if ( iargc() /= 2 ) then
+        print *,'usage: txt2dat region type'
+        call exit(-1)
+    end if
+    call getarg(1,reg)
+    call getarg(2,type)
+    if ( reg == 'GLB') then
+        rg = 'gl'
+        region = 'global'
+    else if ( reg == 'NH') then
+        rg = 'nh'
+        region = 'northern hemisphere'
+    else if ( reg == 'SH') then
+        rg = 'sh'
+        region = 'southern hemisphere'
+    else
+        write(0,*) 'error: expected region GLB, NH or SH not ' ,trim(region)
+        call exit(-1)
+    end if
+    if ( type == 'Ts' ) then
+        tp = 'ts'
+    else if ( type == 'Ts+dSST' ) then
+        tp = 'al'
+    else
+        write(0,*) 'error: expected type Ts or Ts+dSST, not ',trim(type)
+        call exit(-1)
+    end if
+
+    line = '# source :: GISTEMP Team, 2018: GISS Surface Temperature Analysis '// &
+            '(GISTEMP). NASA Goddard Institute for Space Studies. '// &
+            'Dataset accessed'
+    history = '# history :: retrieved and converted'
+    call date_and_time(values=idatum)
+    write(line(len_trim(line)+2:),'(i4,a,i2.2,a,i2.2)') idatum(1),'-',idatum(2),'-',idatum(3)
+    write(line(len_trim(line)+2:),'(i2,a,i2.2,a,i2.2)') idatum(5),':',idatum(6),':',idatum(7)
+    write(history(len_trim(history)+2:),'(i4,a,i2.2,a,i2.2)') idatum(1),'-',idatum(2),'-',idatum(3)
+    write(history(len_trim(history)+2:),'(i2,a,i2.2,a,i2.2)') idatum(5),':',idatum(6),':',idatum(7)
+
+    open(1,file=trim(reg)//'.'//trim(type)//'.txt',status='old')
+    open(2,file='giss_'//tp//'_'//rg//'_m.dat')
+    open(3,file='giss_'//tp//'_'//rg//'_s.dat')
+    open(4,file='giss_'//tp//'_'//rg//'_a.dat')
+    do i=2,4
+        if ( tp == 'Ts' ) then
+            write(i,'(a)') '# GISS Surface Temperature Analysis, ' &
+                //trim(region)//' mean anomalies'
+        else
+            write(i,'(a)') '# GISS Land-Ocean Temperature Index, ' &
+                //trim(region)//' mean anomalies'
+        end if
+        write(i,'(a)') '# Source: <a href="http://data.giss.nasa.gov/gistemp/">NASA/GISS</a>'
+        write(i,'(a)')'# Ta [K] '//trim(region)//' temperature anomaly'
+        write(i,'(a)') '# title :: GISTEMP temperature anomalies reletive to 1951-1980'
+        write(i,'(a)') '# institution :: NASA/GISS'
+        write(i,'(a)') '# contact :: https://www.giss.nasa.gov/staff/rruedy.html'
+        write(i,'(a)') '# references :: Hansen, J., R. Ruedy, M. Sato, and K. Lo, 2010: '// &
+            'Global surface temperature change, Rev. Geophys., 48, RG4004, doi:10.1029/2010RG000345'
+        write(i,'(a)') trim(line)
+        write(i,'(a)') '# source_url :: http://data.giss.nasa.gov/gistemp/'
+        write(i,'(a)') trim(history)
+    end do
+
+    mdata = 3e33
+    sdata = 3e33
+    adata = 3e33
+100 continue
+    read(1,'(a)',end=800,err=900) line
+    if ( line(1:1) /= '1' .and. line(1:1) /= '2' ) goto 100
+110 continue
+!!!        print *,trim(line)
+    i = index(line,'*****')
+    if ( i /= 0 ) then
+        line(i:i+4) = ' 999 '
+        goto 110
+    end if
+    i = index(line,'**** ')
+    if ( i /= 0 ) then
+        line(i:i+4) = ' 999 '
+        goto 110
+    end if
+    i = index(line,' ****')
+    if ( i /= 0 ) then
+        line(i:i+4) = ' 999 '
+        goto 110
+    end if
+    i = index(line,'****')
+    if ( i /= 0 ) then
+        line(i+5:) = line(i+4:)
+        line(i:i+4) = ' 999 '
+        goto 110
+    end if
+    i = index(line,' *** ')
+    if ( i /= 0 ) then
+        line(i:i+4) = ' 999 '
+        goto 110
+    end if
+    i = index(line,' ** ')
+    if ( i /= 0 ) then
+        line(i+5:) = line(i+4:)
+        line(i:i+4) = ' 999 '
+        goto 110
+    end if
+    i = index(line,' * ')
+    if ( i /= 0 ) then
+        line(i+5:) = line(i+3:)
+        line(i:i+4) = ' 999 '
+        goto 110
+    end if
+    read(line,*,err=900) vals
+    yr = vals(1)
+    if ( lwrite ) then
+        print *,trim(line)
+        print *,vals
+    end if
+    if ( vals(20) /= yr ) then
+        write(0,*) 'error: vals(20) != yr: ',vals(20),yr
+        call exit(-1)
+    end if
+    do j=1,19
+        if ( vals(j) == 999 ) vals(j) = -99990
+    end do
+    do j=1,12
+        mdata(j,yr) = vals(j+1)/100.
+    end do
+    do j=1,4
+        sdata(j,yr) = vals(j+15)/100.
+    end do
+    adata(yr) = vals(14)/100.
+    goto 100
+
+800 continue
+    call printdatfile(2,mdata,12,12,yrbeg,yrend)
+    call printdatfile(3,sdata,4,4,yrbeg,yrend)
+    call printdatfile(4,adata,1,1,yrbeg,yrend)
+     
+    goto 999
+900 write(0,*) 'error reading data, last line was'
+    write(0,*) trim(line)
+
+999 continue
+end program txt2dat

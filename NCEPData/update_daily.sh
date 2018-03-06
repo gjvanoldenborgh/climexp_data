83e369f5c1098efed1d96922eaa06cdbd7db89bc
Geert Jan van Oldenborgh <oldenborgh@knmi.nl>
2018-03-06 11:41:04 +0100

    Better metadata

--- NCEPData/update_daily.sh
+++ NCEPData/update_daily.sh
@@ -8,9 +8,20 @@ do
   if [ $? != 0 -o "$1" = force ]; then
     abbrv=${file#norm.daily.}
     abbrv=${abbrv%%\.*}
+    case $abbrv in
+        aao) source=http://www.cpc.noaa.gov/products/precip/CWlink/daily_ao_index/aao/aao.shtml;;
+        ao)  source=http://www.cpc.noaa.gov/products/precip/CWlink/daily_ao_index/ao.shtml;;
+        nao) source=http://www.cpc.noaa.gov/products/precip/CWlink/pna/nao.shtml;;
+        pna) source=http://www.cpc.noaa.gov/products/precip/CWlink/pna/pna.shtml;;
+    esac
     cat > cpc_${abbrv}_daily.dat <<EOF
-# CPC <a href="ftp://ftp.cpc.ncep.noaa.gov/cwlinks/">$abbrv</a> index
+# CPC <a href="http://www.cpc.noaa.gov/products/precip/CWlink/daily_ao_index/teleconnections.shtml">$abbrv</a> index
 # $abbrv [1] $abbrv index
+# institution :: NOAA/NCEP/CPC
+# source_url :: ftp://ftp.cpc.ncep.noaa.gov/cwlinks/
+# referemces :: $source
+# history :: retrieved `date`
+# climexp_url :: https://climexp.knmi.nl/getindices.cgi?NCEPData/cpc_${abbrv}_daily
 EOF
     sed -e '/-0.99900E+34/d' -e '/\*/d' $file >> cpc_${abbrv}_daily.dat
     $HOME/NINO/copyfiles.sh cpc_${abbrv}_daily.dat

9b403c92bcbef34f9c60a83af703900880fed62a
Geert Jan van Oldenborgh <oldenborgh@knmi.nl>
2018-05-30 14:26:58 +0200

    Dataset stops in 2014...

--- UCARData/update_pdsi.sh
+++ UCARData/update_pdsi.sh
@@ -2,6 +2,7 @@
 
 yr=`date +%Y`
 yr=$((yr-1))
+yr=2014
 ###wget -N ftp://ftp.cdc.noaa.gov/Datasets/dai_pdsi/pdsi.mon.mean.nc
 file=pdsisc.monthly.maps.1850-$yr.fawc=1.r2.5x2.5.ipe=2.nc
 wget -N http://www.cgd.ucar.edu/cas/catalog/climind/$file.gz
@@ -11,4 +12,6 @@ ncatted -a units,lon,a,c,"degrees_east" -a units,lat,a,c,"degrees_north" \
     -a axis,lon,a,c,"X" -a axis,lat,a,c,"Y" \
     -a units,time,a,c,"years since 0000-01-01 0:00" \
     -a units,sc_PDSI_pm,a,c,"1" $newfile
+file=$newfile
+. $HOME/climexp/add_climexp_url_field.cgi
 $HOME/NINO/copyfiles.sh pdsi.mon.mean.nc $newfile

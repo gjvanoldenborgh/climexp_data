7f36e9f2b54f96c01f70187c35e5ef9e7c829345
Geert Jan van Oldenborgh <oldenborgh@knmi.nl>
2018-07-03 14:04:52 +0200

    I thought they had fixed the wrong units on the latitude axis, but no...

--- GPCCData/update8.sh
+++ GPCCData/update8.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # download GPCC v8
-download=true
+download=false # true
 base=ftp://ftp.dwd.de/pub/data/gpcc/full_data_2018/
 for res in 25 10 05 025
 do
@@ -9,6 +9,7 @@ do
         wget -N $base/$file.gz
         gunzip -c $file.gz > $file
     fi
+    ncatted -a units,lat,m,c,"degrees_north" $file
     cdo -r -f nc4 -z zip selvar,precip $file gpcc_V8_${res}.nc
     cdo -r -f nc4 -z zip selvar,numgauge $file gpcc_V8_${res}_n.nc
     cdo -r -f nc4 -z zip ifthen gpcc_V8_${res}_n.nc gpcc_V8_${res}.nc gpcc_V8_${res}_n1.nc

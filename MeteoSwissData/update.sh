95c6ed43ff22f51f0661d0ac5a8e7d267b080c62
Geert Jan van Oldenborgh <oldenborgh@knmi.nl>
2018-12-17 16:07:32 +0100

    Improved metadata

--- MeteoSwissData/update.sh
+++ MeteoSwissData/update.sh
@@ -5,7 +5,8 @@ for region in swiss north.low north.high south
 do
     wget -q -N ${base}${version}-$region.txt
     egrep -v '^[12]' ${version}-$region.txt | fgrep -v 'djf' | tr -d '\r' | egrep -v '^[ \t]*$' \
-        | sed -e 's/^/# /' -e 's/: / :: /' -e 's/DOI: /doi:/' > swiss_$region.dat
+        | sed -e 's/^/# /' -e 's/: / :: /' -e 's/DOI: /doi:/' -e 's/Federal/institution :: Federal/' \
+        -e 's/Area/title :: Area/' > swiss_$region.dat
     echo "# T [Celsius] Swiss ${region}-averaged temperature" >> swiss_$region.dat
     egrep '^[12]'  ${version}-$region.txt | cut -f 1-13 -d '	' | sed -e 's/NA/-999.9/g' >> swiss_$region.dat
 done

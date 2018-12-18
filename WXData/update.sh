4007a479696757423a803d54bbbe3e1b65760a19
Geert Jan van Oldenborgh <oldenborgh@knmi.nl>
2018-12-18 09:05:56 +0100

    Data has moved to another domain.

--- WXData/update.sh
+++ WXData/update.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Maue's ACE series
 #
-wget -q -N http://wx.graphics/tropical/global_ace.dat
+wget -q -N http://policlimate.com/tropical/global_ace.dat
 for region in nepac natl wpac nio nh sh global; do
     case $region in
         nepac) col=2;name="Northeast Pacific";;
@@ -17,8 +17,8 @@ for region in nepac natl wpac nio nh sh global; do
 # Maue Tropical Accumulated Cycone Energy
 # references :: Maue, 2011. Recent historically low global tropical cyclone activity, GRL, 38, L14803, doi:10.1029/2011GL047711
 # references :: <a href="http://policlimate.com/tropical/maue_2011gl047711-readme.txt">aux.mat.</a>
-# source :: <a href="http://wx.graphics/tropical/">wx.graphics</a>
-# source_url :: http://wx.graphics/tropical/global_ace.dat
+# source :: <a href="http://policlimate.com/tropical/">wx.graphics</a>
+# source_url :: http://policlimate.com/tropical/global_ace.dat
 # ACE [10^4 kn^2] Accumulated Cycone Energy for the $name
 EOF
     tail -n +5 global_ace.dat | cut  -d "	" -f 1,$col > aap.dat

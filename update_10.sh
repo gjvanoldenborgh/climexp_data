28c527400769a8c13938063d7d55019dc92351b0
Geert Jan van Oldenborgh <oldenborgh@knmi.nl>
2019-02-05 11:06:39 +0100

    Got rid of all the references to my workstation zuidzee

--- update_10.sh
+++ update_10.sh
@@ -12,8 +12,8 @@ else
 fi
 
 echo @@@ GISS
-(ssh zuidzee "cd NINO/NASAData; ./update.sh |& tee update.log")
-(ssh zuidzee "cd NINO/NASAData; ./update_fields.sh |& tee update_fields.log")
+(cd NINO/NASAData; ./update.sh | 2>&1 tee update.log)
+(cd NINO/NASAData; ./update_fields.sh | 2>&1 tee update_fields.log)
 
 echo "@@@ University of Colorado (sealevel)"
 (cd CUData; ./update_indices.sh | 2>&1 tee update.log)
@@ -26,7 +26,7 @@ echo "@@@ other sea level indices"
 (cd CSIROData; ./update.sh | 2>&1 tee update.log)
 
 ###echo @@@ LOD  moved
-###(ssh zuidzee "cd NINO/IERSData; ./update.sh |& tee update.log")
+###cd NINO/IERSData; ./update.sh | 2>&1 tee update.log)
 
 echo @@@ sunspots
 (cd SIDCData; ./update.sh | 2>&1 tee update.log)
@@ -50,8 +50,8 @@ echo @@@ PMOD
 (cd PMODData; ./update_indices.sh | 2>&1 tee update.log)
 
 echo @@@ Rutgers
-(ssh zuidzee "cd NINO/RutgersData; ./update.sh |& tee update.log")
-(ssh zuidzee "cd NINO/RutgersData; ./update_fields.sh |& tee update.log")
+(cd NINO/RutgersData; ./update.sh | 2>&1 tee update.log)
+(cd NINO/RutgersData; ./update_fields.sh | 2>&1 tee update.log)
 
 echo @@@ UW
 (cd UWData; ./update.sh | 2>&1 tee update.log)
@@ -146,12 +146,12 @@ echo @@@ NCEP/NCAR daily
 (cd NCEPNCAR40; ./update_daily.sh | 2>&1 tee update_daily.log)
 
 echo @@@ ERA-interim
-(ssh zuidzee "cd NINO/ERA-interim; ./update_indices.sh |& tee update_indices.log")
-(ssh zuidzee "cd NINO/ERA-interim; ./update.sh |& tee update.log")
+(cd NINO/ERA-interim; ./update_indices.sh | 2>&1 tee update_indices.log)
+(cd NINO/ERA-interim; ./update.sh | 2>&1 tee update.log)
 
 echo @@@ ERA5
-(ssh zuidzee "cd NINO/ERA5; ./update_monthly.sh |& tee update_monthly.log")
-(ssh zuidzee "cd NINO/ERA5; ./update_daily.sh |& tee update_daily.log")
+(cd NINO/ERA5; ./update_monthly.sh | 2>&1 tee update_monthly.log)
+(cd NINO/ERA5; ./update_daily.sh | 2>&1 tee update_daily.log)
 
 echo @@@ GHCN-D
 (cd GDCNData; ./update.sh  | 2>&1 tee update.log )
@@ -160,10 +160,10 @@ echo @@@ SSMI
 (cd SSMIData; ./update.sh | 2>&1 tee update.log)
 
 echo @@@ NCEP daily SST
-(ssh zuidzee "cd NINO/NCEPData; ./update_sstoiv2_daily.sh |& tee update_sstoiv2_daily.log")
+(cd NINO/NCEPData; ./update_sstoiv2_daily.sh | 2>&1 tee update_sstoiv2_daily.log)
 
 echo @@@ PRISM
-(ssh zuidzee "cd NINO/PRISMData; ./update.sh |& tee update.log")
+(cd NINO/PRISMData; ./update.sh | 2>&1 tee update.log)
 
 ###echo @@@ Berkeley
 ###(cd BerkeleyData; ./update.sh | 2>&1 tee update.log)

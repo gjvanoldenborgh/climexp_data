436e582c966ecd3714aea41cfe7f9a67854777ad
Geert Jan van Oldenborgh <oldenborgh@knmi.nl>
2019-02-28 17:15:23 +0000

    No more detours to my workstation.

--- update_25.sh
+++ update_25.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
-if [ $HOST != bvlclim.knmi.nl ]; then
-  echo "Are you sure you want to run this script on $HOST?"
-  read yesno
-  if [ $yesno != yes -a $yesno != y ]; then
-    exit
-  fi
-else
-  scp -q gjvo@shell.xs4all.nl:WWW/ip.txt $HOME/etc/ip2.txt
-fi
 echo @@@ GISS
-(ssh zuidzee "cd NINO/NASAData; ./update.sh |& tee update.log")
-(ssh zuidzee "cd NINO/NASAData; ./update_fields.sh |& tee update_fields.log")
+(cd NASAData; ./update.sh | 2>&1 tee update.log)
+(cd NASAData; ./update_fields.sh | 2>&1 tee update_fields.log)
 echo @@@ NCDC
 (cd NCDCData; ./update_indices.sh | 2>&1 tee update_indices.log)
 (cd NCDCData; ./update_fields.sh | 2>&1 tee update_fields.log)
@@ -32,9 +23,9 @@ echo @@@ NODC
 echo @@@ JAMSTEC
 (cd JAMSTECData; ./update.sh | 2>&1 tee update.log )
 echo @@@ Rutgers
-(ssh zuidzee "cd NINO/RutgersData; ./update.sh |& tee update.log")
-(ssh zuidzee "cd NINO/RutgersData; ./update_fields.sh |& tee update.log")
+(cd RutgersData; ./update.sh | 2>&1 tee update.log)
+(cd RutgersData; ./update_fields.sh | 2>&1 tee update.log)
 echo @@@ MERRA
-(ssh zuidzee "cd NINO/MERRA; ./update_fields.sh |& tee update_fields.log")
+(cd MERRA; ./update_fields.sh | 2>&1 tee update_fields.log)
 
 echo @@@ finished @@@

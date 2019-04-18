ba863b7c9ceb0f955d569cef4401c705b3fd450f
Geert Jan van Oldenborgh <oldenborgh@knmi.nl>
2019-04-18 17:40:25 +0200

    Some postprocessing of ghe file Jonathan Palmer sent me.

--- UNSWData/update.sh
+++ UNSWData/update.sh
@@ -8,7 +8,8 @@ ncpdq -O -a time,lat,lon $file /tmp/$file
 cdo -r -f nc4 -z zip settaxis,1500-06-01,0:00,1year /tmp/$file $file
 # put the correct units on the lat-lon axes
 ncatted -h -a units,lon,m,c,'degrees_east' -a units,lat,m,c,'degrees_north' $file
-# and a title
-ncatted -h -a Title,global,a,c,"Reconstructed scPDSI for Australia/New Zealand Drought Atlas (ANZDA v1.0)" $file
+# add a title and contact
+ncatted -h -a Title,global,a,c,"Reconstructed scPDSI for Australia/New Zealand Drought Atlas (ANZDA v1.0)" \
+    -a contact,global,a,c,"j.palmer@unsw.edu.au" $file
 # and add superfluous metadata
 . $HOME/climexp/add_climexp_url_field.cgi

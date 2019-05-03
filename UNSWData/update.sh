a7e859a39db866fe5db20d2f5cc09a9bb23e50e2
Geert Jan van Oldenborgh <oldenborgh@knmi.nl>
2019-05-03 10:09:13 +0100

    Added more metadata.

--- UNSWData/update.sh
+++ UNSWData/update.sh
@@ -10,6 +10,9 @@ cdo -r -f nc4 -z zip settaxis,1500-06-01,0:00,1year /tmp/$file $file
 ncatted -h -a units,lon,m,c,'degrees_east' -a units,lat,m,c,'degrees_north' $file
 # add a title and contact
 ncatted -h -a Title,global,a,c,"Reconstructed scPDSI for Australia/New Zealand Drought Atlas (ANZDA v1.0)" \
-    -a contact,global,a,c,"j.palmer@unsw.edu.au" $file
+    -a contact,global,a,c,"j.palmer@unsw.edu.au" \
+    -a references,global,a,c,"Palmer, J., E. R. Cook, C. S. M. Turney, K. Allen, P. Fenwick, B. I. Cook, A. O'Donnell, J. Lough, P. Grierson, and P. Baker (2015), Drought variability in the eastern Australia and New Zealand summer drought atlas (ANZDA, CE 1500-2012) modulated by the Interdecadal Pacific Oscillation, Environ. Res. Lett., 10(12), 124002-13, doi:10.1088/1748-9326/10/12/124002" \
+    -a source,global,a,c,"https://www1.ncdc.noaa.gov/pub/data/paleo/treering/reconstructions/australia/palmer2015pdsi/readme-palmer2015anzda.txt" \
+    $file
 # and add superfluous metadata
 . $HOME/climexp/add_climexp_url_field.cgi

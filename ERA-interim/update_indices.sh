3a3c25c6f44e1608bcfdb1d5e657be0a926ca0a7
Geert Jan van Oldenborgh <oldenborgh@knmi.nl>
2019-05-06 20:24:18 +0100

    Also try last month's data if this month is not yet available.

--- ERA-interim/update_indices.sh
+++ ERA-interim/update_indices.sh
@@ -3,9 +3,21 @@ yr=`date +%Y -d now`
 mo=`date +%m -d now`
 yr1=`date +%Y -d '1 month ago'`
 mo1=`date +%m -d '1 month ago'`
+yr2=`date +%Y -d '2 months ago'`
+mo2=`date +%m -d '2 months ago'`
 base=https://climate.copernicus.eu/sites/default/files/${yr}-${mo}/
 file=ts_1month_anomaly_Global_ei_2T_${yr1}${mo1}.csv
 wget --no-check-certificate -N $base/$file
+if [ ! -s $file ]; then
+    file1=$file
+    base=https://climate.copernicus.eu/sites/default/files/${yr1}-${mo1}/
+    file=ts_1month_anomaly_Global_ei_2T_${yr2}${mo2}.csv
+    wget --no-check-certificate -N $base/$file
+    if [ ! -s $file ]; then
+        echo "$0: error: cannot find $file1 or $file"
+        exit -1
+    fi
+fi
 for ext in gl eu; do
     case $ext in
         gl) region="the world";col=2;;
@@ -15,7 +27,7 @@ for ext in gl eu; do
     outfile=erai_t2m_$ext.dat
     cat > $outfile <<EOF
 # t2m [K} surface air temperature anomalies relative to 1981-2010
-# from the ERA-interim reanalysis (first-odrer corrected for the inhomogeneity in 2001)
+# from the ERA-interim reanalysis (first-order corrected for the inhomogeneity in 2001)
 # averaged over $region
 # institution :: Copernicus Climate Change Service
 # contact :: Adrian.Simmons@ecmwf.int

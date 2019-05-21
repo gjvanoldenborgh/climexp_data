699b49106e3f85dd6f748780105dc94b566c6f84
Geert Jan van Oldenborgh <oldenborgh@knmi.nl>
2019-05-21 16:29:41 +0200

    Switch to year-on-year difference rather than centred derivatives.

--- CDIACData/plotco2.sh
+++ CDIACData/plotco2.sh
@@ -6,7 +6,7 @@ diffdat maunaloa_f_mean1.dat 2 > diff_maunaloa_mean1.dat
 diffdat co2_annual.dat 2 > diff_co2_annual.dat
 for scen in RCP3PD RCP45 RCP6 RCP85
 do
-    [ ! -s diff${scen}_CO2.dat ] && diffdat ${scen}_CO2.dat 3 > diff_${scen}_CO2.dat
+    [ ! -s diff${scen}_CO2.dat ] && diffdat ${scen}_CO2.dat 2 > diff_${scen}_CO2.dat
 done
 gnuplot plotco2.gnuplot
 gnuplot plotdco2.gnuplot

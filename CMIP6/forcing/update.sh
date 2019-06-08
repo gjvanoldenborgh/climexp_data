fd00475d5a39b6c20dbfe642ca0f2e2f45c7acee
Geert Jan van Oldenborgh <oldenborgh@knmi.nl>
2019-06-08 22:10:06 +0200

    Oops.

--- CMIP6/forcing/update.sh
+++ CMIP6/forcing/update.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 base=http://solarisheppa.geomar.de/solarisheppa/sites/default/files/data/cmip6
 file=solarforcing-ref-mon_input4MIPs_solar_CMIP_SOLARIS-HEPPA-3-2_gn_18500101-22991231.nc
-wget -O -N $base/$file.gz
+wget -N $base/$file.gz
 gunzip -c $file.gz > $file
 for var in tsi f107 ap kp; do
     ncks -O -v $var $file cmip6_$var.nc

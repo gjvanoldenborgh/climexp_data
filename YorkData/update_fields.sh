bc6ad096fa6b4d08aea0561e5af6288772d6cc0e
Geert Jan van Oldenborgh <oldenborgh@knmi.nl>
2019-07-19 10:16:47 +0200

    Added HadSST4 products.

--- YorkData/update_fields.sh
+++ YorkData/update_fields.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 base=http://www-users.york.ac.uk/~kdc3/papers/coverage2013
-for file in had4_krig_v2_0_0.nc
+for file in had4_krig_v2_0_0.nc had4sst4_krig_v2_0_0.nc
 do
     cp $file $file.old
     wget -N -q $base/$file.gz
--- YorkData/update_indices.sh
+++ YorkData/update_indices.sh
@@ -1,14 +1,24 @@
 #!/bin/sh
 base=http://www-users.york.ac.uk/~kdc3/papers/coverage2013
-for file in had4_krig_v2_0_0.txt
+for file in had4_krig_v2_0_0.txt had4sst4_krig_v2_0_0.txt
 do
+    case $file in
+        had4_krig_v2_0_0.txt) name="HadCRUT4";;
+        had4sst4_krig_v2_0_0.txt) name="HadCRUT4/HadSST4";;
+        *) echo "$0: error: unknown file name $file"; exit -1;;
+    esac
     wget -N -q $base/$file
+    c=`file $file | fgrep -c gzip`
+    if [ $c != 0 ]; then
+        mv $file $file.gz
+        gunzip $file.gz
+    fi
     f=${file%.txt}_gl.dat
     cat > $f <<EOF
 # <a href="http://www-users.york.ac.uk/~kdc3/papers/coverage2013/series.html">Cowtan and Way</a> global mean temperature, HadCRUT4 in-filled with kriging.
-# Tglobal [K] HadCRUT4 global mean T2m/SST in-filled with kriging
+# Tglobal [K] $name global mean T2m/SST in-filled with kriging
 # institution :: University of York, University of Ottawa.
-# title ::  HadCRUT4 in-filled with kriging
+# title ::  $name in-filled with kriging
 # references :: Cowtan, K., & Way, R. G. (2014). Coverage bias in the HadCRUT4 temperature series and its impact on recent temperature trends. Quarterly Journal of the Royal Meteorological Society.
 # history :: retrieved `date`
 # source_url :: $base/$file

aaa9bd1690bec24bcb4abd67303dfefa346af445
Geert Jan van Oldenborgh <oldenborgh@knmi.nl>
2019-08-20 16:37:13 +0200

    Updated Dangendorf et al to their 2019 article.

--- SiegenData/csv2dat.sh
+++ SiegenData/csv2dat.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
+echo "# 20th century global mean sea level by <a href=\"https://www.nature.com/articles/s41558-019-0531-8\">Dangendorf et al, 2019</a>" > ssh_dangendorf_mo.dat
+echo "# ssh [mm] global mean sea level" >> ssh_dangendorf_mo.dat
+tail -n +2 DataDangendorf2019.txt | cut -b 1-35 | sed -e 's/1900    /1900.000/' >> ssh_dangendorf_mo.dat
+
+echo "# 20th century global mean sea level by <a href=\"https://www.nature.com/articles/s41558-019-0531-8\">Dangendorf et al 2019</a>" > dssh_dangendorf_mo.dat
+echo "# dssh [mm] 95% CI uncertainty on global mean sea level" >> dssh_dangendorf_mo.dat
+tail -n +2 DataDangendorf2019.txt | cut -b 1-18,36-54 | tr ';,' ' .' >> dssh_dangendorf_mo.dat
+$HOME/NINO/copyfilesall.sh ssh_dangendorf_mo.dat dssh_dangendorf_mo.dat
+
 echo "# 20th century global mean sea level by <a href=\"http://www.pnas.org/content/114/23/5946.abstract\">Dangendorf et al</a>" > ssh_dangendorf.dat
 echo "# ssh [mm] global mean sea level" >> ssh_dangendorf.dat
 tail -n +2 GMSL_Dangendorf.csv | cut -d ';' -f 1-2 | tr ';,' ' .' >> ssh_dangendorf.dat
--- SiegenData/update.sh
+++ SiegenData/update.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+patchseries ssh_dangendorf_mo.dat ../AVISOData/ssh_aviso.dat bias > ssh_dangendorf_mo_extended.dat
 patchseries ssh_dangendorf.dat ../AVISOData/ssh_aviso_annual.dat bias > ssh_dangendorf_extended.dat
-$HOME/NINO/copyfilesall.sh  ssh_dangendorf_extended.dat
+$HOME/NINO/copyfilesall.sh ssh_dangendorf_extended.dat ssh_dangendorf_mo_extended.dat

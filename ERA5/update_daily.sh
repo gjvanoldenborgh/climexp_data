95415a2b20c5c8388bdb1e7af91fedc2ae839c08
Geert Jan van Oldenborgh <oldenborgh@knmi.nl>
2019-09-12 12:01:22 +0200

    Added land/sea mask

--- ERA5/update_daily.sh
+++ ERA5/update_daily.sh
@@ -5,8 +5,6 @@ yrnow=`date +%Y -d "2 months ago"`
 cdo="cdo -r -f nc4 -z zip"
 sourcedir=/net/pc170547/nobackup_2/users/sager/ERA5
 vars=`ls $sourcedir/2010/day/era5_201001* | sed -e "s@$sourcedir/2010/day/era5_201001_@@" -e 's/\..*$//'`
-echo "DEBUG: only take the non-flux parametres, the others are wrong 21-aug-2019"
-vars="t2m msl z500 q500 sfcWind sfcWindmax sp" # these are OK 21-aug-2019
 filelist=""
 for var in $vars; do
     if [ $var != 3d ]; then
@@ -24,11 +22,12 @@ for var in $vars; do
 
 #       global 0.5 degree version, regional 0.25 degree versions
 
-        regions="05 eu"
+        regions="05 eu af"
         for region in $regions; do
             case $region in
                 05) lon1=;lon2=;lat1=;lat2=;;
                 eu) lon1=-30;lon2=40;lat1=30;lat2=75;;
+                af) lon1=-18;lon2=52;lat1=-35;lat2=38;;
                 *) echo "unknown region $region"; exit -1;;
             esac
             mkdir -p $region/$var
@@ -53,7 +52,7 @@ for var in $vars; do
                 file=era5_${var}_daily_$region.nc
             fi
             if [ $lastfile -nt $file -o "$1" = force ]; then
-                echo $$cdo copy $files_region $file
+                echo $cdo copy $files_region $file
                 $cdo copy $files_region $file
                 ncatted -a title,global,c,c,"ERA5 reanalysis, https://www.ecmwf.int/en/forecasts/datasets/reanalysis-datasets/era5" $file
                 . $HOME/climexp/add_climexp_url_field.cgi
@@ -62,6 +61,11 @@ for var in $vars; do
                     ncatted -a units,$var,a,c,"m/s" $file
                 fi
                 $HOME/NINO/copyfiles.sh $file
+            fi
+            lsm=era5_000000_lsm_$region.nc
+            if [ ! -s $lsm ]; then
+                $cdo sellonlatbox,$lon1,$lon2,$lat1,$lat2 era5_000000_lsm_025.nc $lsm
+                $HOME/NINO/copyfiles.sh $lsm
             fi
         done
     fi

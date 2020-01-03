1b5ffdb6c2fe548b1317a1305ffaf626b57157c1
Geert Jan van Oldenborgh <oldenborgh@knmi.nl>
2020-01-03 15:32:37 +0100

    Moved to new data & documentation location. Use old series 1947-2003 that is no longer on download site.

--- SRMPData/update.sh
+++ SRMPData/update.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-wget -q -N ftp://ftp.geolab.nrcan.gc.ca/data/solar_flux/monthly_averages/solflux_monthly_average.txt
-cat  > solarradioflux.dat <<EOF
+source_url=ftp://ftp.seismo.nrcan.gc.ca/spaceweather/solar_flux/monthly_averages/solflux_monthly_average.txt
+wget -q -N $source_url
+cat > solarradioflux_new.dat <<EOF
 # Observed solar radio flux at 10.7cm from <a href="http://www.spaceweather.ca/sx-eng.php">Space Weather Canada</a>
 # F10.7 [sfu] 10.7cm flux density
-# institution :: NRC-GNRC Canada
-# references :: https://www.nrc-cnrc.gc.ca/eng/solutions/advisory/solar_weather_monitoring.html
-# source_url :: ftp://ftp.geolab.nrcan.gc.ca/data/solar_flux/monthly_averages/solflux_monthly_average.txt
+# institution :: NRC-CNRC Canada
+# references :: https://www.spaceweather.gc.ca/solarflux/sx-3-en.php
+# source_url :: $source_url
 # contact :: ken.tapping@nrc-cnrc.gc.ca
 # history :: retrieved `date`
 # climexp_url :: http://climexp.knmi.nl/getinidices.cgi?solarradioflux
 EOF
-tail -n +3 solflux_monthly_average.txt | cut -b 1-25 >> solarradioflux.dat
+tail -n +3 solflux_monthly_average.txt | fgrep -v 9325021.54 | cut -b 1-25 >> solarradioflux_new.dat 
+patchseries solarradioflux_new.dat solarradioflux_org.dat > solarradioflux.dat
 $HOME/NINO/copyfilesall.sh solarradioflux.dat

5d6e5bc0e233f2a118954eac4d12238fe510cbf8
Geert Jan van Oldenborgh <oldenborgh@knmi.nl>
2020-03-26 20:20:29 +0100

    Get sign correct.

--- NCEPNCAR40/make_nao.sh
+++ NCEPNCAR40/make_nao.sh
@@ -2,7 +2,8 @@
 if [ ! -s eof_nao_ncepncar.nc ]; then
     eof slp.mon.mean.nc 1 mon 12 sel 4 anomal lon1 -90 lon2 30 lat1 0 lat2 90 end 2018 eof_nao_ncepncar.nc
     fi
-patternfield slp.mon.mean.nc eof_nao_ncepncar.nc eof1 12 > nao_raw.dat
+patternfield slp.mon.mean.nc eof_nao_ncepncar.nc eof1 12 > nao_raw1.dat
+scaleseries -1 nao_raw1.dat > nao_raw.dat
 cat <<EOF > snao_ncepncar.dat
 # NAO index (PC of first EOF of SLP over 0-90N, 90W-30E December-March months)
 # based on the NCEP/NCAR R1 reanalysis sea-level pressure reconstruction

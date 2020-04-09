c350ebbe8781c97b63e56fbded47cd5ba0961f96
Geert Jan van Oldenborgh <oldenborgh@knmi.nl>
2020-04-09 09:04:12 +0200

    Added (maybe wrongly defined) Southern Annular Mode.

--- NCEPNCAR40/update_indices.sh
+++ NCEPNCAR40/update_indices.sh
@@ -1,4 +1,14 @@
-#!/bin/sh
+#!/bin/bash
+
+# SAM
+
+get_index slp.mon.mean.nc 0 360 -40 -40 nearest > slp40.dat
+get_index slp.mon.mean.nc 0 360 -65 -65 nearest > slp65.dat
+normdiff slp40.dat slp65.dat none none > sam_ncepncar.dat
+$HOME/NINO/copyfiles.sh sam_ncepncar.dat
+
+# Cathy's QBO
+
 cp qbo.data qbo.data.old
 wget -N --no-check-certificate https://www.esrl.noaa.gov/psd/data/correlation/qbo.data
 mv nqbo.dat nqbo.dat.old
